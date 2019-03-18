5c3ad32db9ef14ad45f0610b60d1a340e3ffb0e8
UchihaPdx <2323166893@qq.com>
2019-03-18 11:47:28 +0800

    第一次提交

---
+++ src/main/java/com/hr/uchiha/UchihaApplication.java
@@ -0,0 +1,13 @@
+package com.hr.uchiha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UchihaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UchihaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hr/uchiha/UchihaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hr.uchiha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UchihaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
